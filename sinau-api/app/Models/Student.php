d3501f66f0ece5c689206d9801a79f139dd57ca1
Dokter Komputer <dataprojectobenk@gmail.com>
2024-01-25 13:21:07 +0700

    edit env

--- sinau-api/app/Models/Student.php
+++ sinau-api/app/Models/Student.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class Student extends Model
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+class Student extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory,Notifiable,HasApiTokens;
+
+
+    // belongsto 
+    public function ayah(){
+        return $this->belongsTo(Parent::class,"ayah_id","id");
+    }
+    public function ibu(){
+        return $this->belongsTo(Parent::class,"ibu_id","id");
+    }
+    public function wali(){
+        return $this->belongsTo(Parent::class,"wali_id","id");
+    }
+    public function kelas(){
+        return $this->belongsTo(Group::class,"group_id","id");
+    }
 }
